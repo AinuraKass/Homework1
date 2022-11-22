ef83adb2a2d96e65a5afbbb241c5c552887fe968
Ainura <ainur.kassymzhanova@gmail.com>
2022-11-23 00:21:28 +0600

    lesson2

---
+++ Lesson2/src/main/java/OneOrBothRectangleSideIsNegativeException.java
@@ -0,0 +1,5 @@
+public class OneOrBothRectangleSideIsNegativeException extends RuntimeException {
+    public OneOrBothRectangleSideIsNegativeException(String message) {
+        super(message);
+    }
+}
