db7b565a7bb8611a344baa3a8054b40582aa308a
Ainura <ainur.kassymzhanova@gmail.com>
2023-01-02 01:25:58 +0600

    hm9

---
+++ lesson9/src/main/java/lambda/CheckAble.java
@@ -0,0 +1,13 @@
+package lambda;
+
+@FunctionalInterface
+public interface CheckAble {
+    boolean test(Animal animal);
+
+    default void test1() {
+        System.out.println("default");
+    }
+
+    @Override
+    String toString();
+}
